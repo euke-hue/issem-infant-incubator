33e3efb2ac0ed62cb097bf160f43d77f12d731a8
euke-hue <69552368+euke-hue@users.noreply.github.com>
2022-05-10 20:49:46 -0700

    Update infinc.py

--- infinc.py
+++ infinc.py
@@ -44,7 +44,7 @@ class SimpleHeatGenerator :
     def getOutput(self) :
         if self.thermometer :
             if self.thermometer.getTemperature() < self.setTemperature :
-                print(self.power)
+                print("INFINC - the output from heater ran!!")
                 return self.power
         return 0
 
